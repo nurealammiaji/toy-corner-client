cafeed51fbcb6f38d5ef2d0132fdc81d9eb72e2a
nurealammiaji <nurealam.miaji@gmail.com>
2023-11-22 02:34:39 +0600

    blog post added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,19 @@
 
+import { useState } from 'react';
 import DynamicTitle from '../DynamicTitle/DynamicTitle';
+import { useEffect } from 'react';
+import Post from './Post';
 
 const Blog = () => {
+
+    const [posts, setPosts] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/blog')
+            .then(res => res.json())
+            .then(data => setPosts(data))
+    })
+
     return (
         <div>
             <DynamicTitle title="Blog"></DynamicTitle>
@@ -9,6 +21,22 @@ const Blog = () => {
                 <h2 className='w-full mx-auto text-3xl font-bold md:w-6/12 text-primary divider'>Blog</h2>
             </div>
             <br /><br />
+            {
+                (posts) ?
+                    <div className='grid md:grid-cols-2 gap-10'>
+                        {
+                            posts.map(post => <Post key={post._id} post={post}></Post>)
+                        }
+                    </div> :
+                    <>
+                        <div className="flex flex-col gap-4 w-52">
+                            <div className="skeleton h-32 w-full"></div>
+                            <div className="skeleton h-4 w-28"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </div>
+                    </>
+            }
         </div>
     );
 };
--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -1,9 +1,24 @@
 
 
-const Post = () => {
+const Post = ({ post }) => {
+
+    const { _id, title, author, authorEmail, authorImage, image, details } = post;
+
     return (
         <div>
-            
+            <div className="w-full shadow-xl card card-compact bg-base-100">
+                <figure><img src={image} className="h-[300px] w-full" alt="Blog" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <p className="text-gray-500 badge shadow-inner font-thin"><span className="mr-2">Author:</span> {author}</p>
+                    <br />
+                    <div className="text-base [&>*]:mt-3 border-l-4 border-primary pl-8 p-2 card shadow-inner text-justify">
+                        <p>{details?.one}</p>
+                        <p>{details?.two}</p>
+                        <p>{details?.three}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
